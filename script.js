85da86921ea6fbd3740d19ec74a88d42fe913bb9
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-16 13:31:12 +0100

    Iteration#3

--- script.js
+++ script.js
@@ -82,3 +82,57 @@ let result = getMedia(users)
 console.log("La media es: " + result)
 
 
+
+
+//ITERACIÓN #3: Mix Fors
+
+//Dado el siguiente javascript usa for of y for in para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa for in para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+//Una vez que accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+
+
+const users2 = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let repeatedSounds = [""]
+var counts = {};
+for (let value of users2) {
+    console.log(users2)
+
+    for (let prop in value.favoritesSounds) {
+        console.log(prop)
+        const words = prop.split(",");
+        console.log(words); 
+        words.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+        console.log(counts)
+    }
+}
+
+
